d3350b10b7a0cad950df0504d6a0f31e139d3a47
wilsonluizz <wilsonluiz31051991@gmail.com>
2022-03-14 16:01:30 -0300

    primeiro commit programa conta

---
+++ ProgramBanco/src/application/Program.java
@@ -0,0 +1,49 @@
+package application;
+import java.util.Scanner;
+
+import entidades.Contapf;
+
+import java.util.Locale;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner teclado = new Scanner(System.in);
+	
+		
+		Contapf conta1 = new Contapf();
+		System.out.println("Criação de conta pessoa física. Digite os dados do titular:");
+		System.out.println("Nome: ");
+		conta1.setNomeTitular(teclado.nextLine());
+		System.out.println("Digite o número da conta com 8 digitos: ");
+		conta1.setNumeroConta(teclado.nextInt());
+		System.out.println("Digite o valor do depósito inicial R$ ");
+		conta1.setValorDeposito(teclado.nextDouble());
+		
+		
+		System.out.println(conta1);
+		
+		System.out.println("Digite [1] para depósito - [2] para saque e [3] para finalizar");
+		int op = teclado.nextInt();
+		
+		double valor;
+		if (op == 1) {
+			System.out.println("Digite o valor para depósito: R$ ");
+			valor = teclado.nextDouble();
+			conta1.opdeposito(valor);
+		}
+		else if (op == 2) {
+			System.out.println("Digite o valor para saque: R$ ");
+			valor = teclado.nextDouble();
+			conta1.opsaque(valor);
+		}
+		
+		System.out.println("Dados atualizados " + conta1);
+		
+		teclado.close();
+		
+
+	}
+
+}
